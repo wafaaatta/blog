57a7b7e0b5535c52846e2e5f6cbcf561356f617c
unknown <wafaaatta04@gmail.com>
2024-04-19 16:52:35 +0200

    css en cours

--- resources/views/layouts/front/header.blade.php
+++ resources/views/layouts/front/header.blade.php
@@ -12,6 +12,18 @@
                                     >
                                         Dashboard
                                     </a>
+                                    <a
+                                        href="{{ url('/dashboard/myposts') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        My Posts
+                                    </a>
+                                    <a
+                                        href="{{ url('/blog') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        All Posts
+                                    </a>
                                 @else
                                     <a
                                         href="{{ route('login') }}"
